0af0a9236cdb9499d8c657da7270b82ea04338de
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-07 00:14:28 +0200

    Add route for finding data.

--- routes/words.js
+++ routes/words.js
@@ -3,4 +3,15 @@ const db = require("../database/crudrepository.js");
 
 const words = express.Router();
 
+words.get("/data/:table", async (req, res) => {
+  let table = req.params.table;
+
+  try {
+    let result = await db.findAll(table);
+    res.send(result);
+  } catch (err) {
+    res.status(err).end();
+  }
+});
+
 module.exports = words;
