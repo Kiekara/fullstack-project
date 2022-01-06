9dbae73645a059912abd4e29e103c14de7c42fdd
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-07 00:16:10 +0200

    Define main route for app.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const words = require("./routes/words.js");
 const app = express();
 const port = 8080;
 
 app.use(express.json());
 
 app.use(cors());
+
+app.use("/language-app", words);
 
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
