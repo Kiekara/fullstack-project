503b311ac5ea5580335683e274ff9422a1f7babe
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-07 16:58:52 +0200

    Add word schema for validating post requests.

--- routes/words.js
+++ routes/words.js
@@ -1,7 +1,36 @@
 const express = require("express");
+const Validator = require("jsonschema").Validator;
 const db = require("../database/crudrepository.js");
 
 const words = express.Router();
+const validator = new Validator();
+
+const wordRowSchema = {
+  minProperties: 2,
+  maxProperties: 4,
+  type: "object",
+  properties: {
+    tag_id: {
+      type: "number",
+      minimum: 1,
+    },
+    word_eng: {
+      type: "string",
+      minLength: 1,
+      maxLength: 100,
+    },
+    word_swe: {
+      type: "string",
+      minLength: 1,
+      maxLength: 100,
+    },
+    word_fin: {
+      type: "string",
+      minLength: 2,
+      maxLength: 100,
+    },
+  },
+};
 
 words.get("/data/:table", async (req, res) => {
   let table = req.params.table;
@@ -18,11 +47,19 @@ words.post("/data/:table", async (req, res) => {
   let table = req.params.table;
   let data = req.body;
 
-  try {
-    let result = await db.save(table, data);
-    res.status(result).send(data);
-  } catch (err) {
-    res.status(err).end();
+  if (table == "words") {
+    let validation = validator.validate(data, wordRowSchema);
+
+    if (validation.errors.length > 0) {
+      res.status(400).send(validation.errors);
+    } else {
+      try {
+        let result = await db.save(table, data);
+        res.status(result).send(data);
+      } catch (err) {
+        res.status(err).end();
+      }
+    }
   }
 });
 
