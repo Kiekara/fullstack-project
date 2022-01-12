78b70a42b02ea634783995819cee22835a01ad85
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-12 21:13:16 +0200

    Change imported router name from words to data.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const words = require("./routes/words.js");
+const data = require("./routes/words.js");
 const app = express();
 const port = 8080;
 
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use("/data", words);
+app.use("/data", data);
 
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
