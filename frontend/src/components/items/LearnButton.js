9b58a3923b5ec51e67fe0bb69b3b37fed0957fed
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 04:59:13 +0200

    Add function to change sort state on click.

--- frontend/src/components/items/LearnButton.js
+++ frontend/src/components/items/LearnButton.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Button } from "@mui/material";
 
-function LearnButton() {
+function LearnButton({ setSort }) {
+  const handleClick = () => {
+    setSort(0);
+  };
+
   return (
     <>
-      <Button variant="contained" size="medium">
+      <Button variant="contained" size="medium" onClick={handleClick}>
         Learn random words
       </Button>
     </>
