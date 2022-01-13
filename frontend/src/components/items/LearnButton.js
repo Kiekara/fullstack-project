ee56f55a1a407d4374d951f3abea921fef362b87
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 05:07:03 +0200

    Add conditional button disabling.

--- frontend/src/components/items/LearnButton.js
+++ frontend/src/components/items/LearnButton.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Button } from "@mui/material";
 
-function LearnButton({ setSort, setLearn }) {
+function LearnButton({ learn, setSort, setLearn }) {
   const handleClick = () => {
     setSort(0);
     setLearn(true);
   };
 
-  return (
+  return !learn ? (
     <>
       <Button variant="contained" size="medium" onClick={handleClick}>
+        Learn random words
+      </Button>
+    </>
+  ) : (
+    <>
+      <Button disabled variant="contained" size="medium" onClick={handleClick}>
         Learn random words
       </Button>
     </>
