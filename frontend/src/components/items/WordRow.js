2ee833033d51282e2656c7a28a876a8addcde339
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 03:33:27 +0200

    Return TextField component per row for translating.

--- frontend/src/components/items/WordRow.js
+++ frontend/src/components/items/WordRow.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { ListItem, ListItemText } from "@mui/material";
+import { ListItem, ListItemText, TextField } from "@mui/material";
 
 function WordRow({ row }) {
   return (
     <>
       <ListItem>
         <ListItemText primary={row.wordEng} />
+        <TextField
+          hiddenLabel
+          variant="filled"
+          size="small"
+          margin="dense"
+          sx={{ width: "50%" }}
+        />
       </ListItem>
     </>
   );
