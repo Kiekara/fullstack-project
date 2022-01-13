50e0e9a5e43a87e22f377b6804fc4c7d8f02d546
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 19:18:07 +0200

    Add submit state reset to handleClick.

--- frontend/src/components/items/BackButton.js
+++ frontend/src/components/items/BackButton.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button } from "@mui/material";
 
-function BackButton({ learn, setSort, setLearn }) {
+function BackButton({ learn, setSort, setLearn, setSubmit }) {
   const handleClick = () => {
     setSort(-1);
     setLearn(false);
+    setSubmit(false);
   };
 
   return !learn ? (
