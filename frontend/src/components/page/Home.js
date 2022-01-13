e357f8718f1d3b629b53a5ee29f5764ed80fb0d3
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 02:08:19 +0200

    Add function for finding data on Home() mount.

--- frontend/src/components/page/Home.js
+++ frontend/src/components/page/Home.js
@@ -1,8 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Home() {
   const [tags, setTags] = useState([]);
   const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    const get = async (path) => {
+      let data = await getData(path);
+
+      if (path === "tags") {
+        setTags(data);
+      } else {
+        setWords(data);
+      }
+    };
+
+    get("tags");
+    get("words");
+  }, []);
+
+  const getData = async (path) => {
+    let response = await fetch(`http://localhost:8080/data/${path}/`, {
+      method: "GET",
+    });
+    let data = await response.json();
+
+    return data;
+  };
 
   return <div></div>;
 }
