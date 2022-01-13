fcd540c8190c5032918129d224b7022657f22b37
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 03:18:11 +0200

    Add word row mapping on return.

--- frontend/src/components/sections/WordList.js
+++ frontend/src/components/sections/WordList.js
@@ -1,7 +1,25 @@
 import React from "react";
+import WordRow from "../items/WordRow";
+import { List } from "@mui/material";
 
-function WordList() {
-  return <div></div>;
+function WordList({ words }) {
+  return (
+    <>
+      <List
+        sx={{
+          bgcolor: "gray",
+          pr: "16px",
+          pl: "16px",
+          border: "1px solid black",
+          borderRadius: "8px",
+        }}
+      >
+        {words.map((row) => (
+          <WordRow row={row} />
+        ))}
+      </List>
+    </>
+  );
 }
 
 export default WordList;
