95cb080130c119e54db8a689c97d4bfb516d7c62
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 01:37:32 +0200

    Add Button component with icon and text.

--- frontend/src/components/items/EditButton.js
+++ frontend/src/components/items/EditButton.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Button } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
 
 function EditButton() {
-  return <div></div>;
+  return (
+    <span style={{ marginRight: "auto", marginLeft: "auto" }}>
+      <Button
+        variant="contained"
+        size="medium"
+        startIcon={<FontAwesomeIcon icon={faEdit} />}
+      >
+        Edit category
+      </Button>
+    </span>
+  );
 }
 
 export default EditButton;
