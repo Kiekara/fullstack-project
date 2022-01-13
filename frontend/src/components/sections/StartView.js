e30e79bc6469f85f72a578fa50540817a2e1a790
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 05:21:37 +0200

    Add text content to welcome users.

--- frontend/src/components/sections/StartView.js
+++ frontend/src/components/sections/StartView.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Box } from "@mui/material";
 
 function StartView() {
-  return <div></div>;
+  return (
+    <>
+      <Box mr={"60px"} ml={"60px"}>
+        <h3>Welcome to my language learning app!</h3>
+        <br />
+        <br />
+        <p>
+          You can start learning languages straight away by selecting a category
+          from the left or just practise with random words
+        </p>
+        <br />
+      </Box>
+    </>
+  );
 }
 
 export default StartView;
