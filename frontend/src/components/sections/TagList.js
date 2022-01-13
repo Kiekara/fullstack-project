967b8bda4fb01fd5067b52a6674f3fd112e4dcfb
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 04:08:21 +0200

    Forward learn state for TagButton() component.

--- frontend/src/components/sections/TagList.js
+++ frontend/src/components/sections/TagList.js
@@ -2,13 +2,18 @@ import React from "react";
 import TagButton from "../items/TagButton";
 import { Stack } from "@mui/material";
 
-function TagList({ tags, setSort }) {
+function TagList({ tags, learn, setSort, setLearn }) {
   return (
     <>
       <Stack spacing={2}>
         <h3>Choose a category</h3>
         {tags.map((tag) => (
-          <TagButton tag={tag} setSort={setSort} />
+          <TagButton
+            tag={tag}
+            learn={learn}
+            setSort={setSort}
+            setLearn={setLearn}
+          />
         ))}
       </Stack>
     </>
