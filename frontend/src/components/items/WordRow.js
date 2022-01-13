39dabdde4b2e1d44327ae25e275c0d87c7aaffae
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 17:27:35 +0200

    Add function for handling TextField change.

--- frontend/src/components/items/WordRow.js
+++ frontend/src/components/items/WordRow.js
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
 import { ListItem, ListItemText, TextField } from "@mui/material";
 
-function WordRow({ row }) {
+function WordRow({ row, index, answers, setAnswers }) {
   const { wordEng, wordFin } = row;
   const [input, setInput] = useState("");
+
+  const handleChange = (event) => {
+    let inputValue = event.target.value;
+    setInput(inputValue);
+
+    if (inputValue === wordFin) {
+      setAnswers(
+        answers.map((answer, idx) => {
+          return idx === index ? true : answer;
+        })
+      );
+    } else {
+      setAnswers(
+        answers.map((answer, idx) => {
+          return idx === index ? false : answer;
+        })
+      );
+    }
+  };
 
   return (
     <>
@@ -16,6 +35,7 @@ function WordRow({ row }) {
           size="small"
           margin="dense"
           sx={{ width: "50%" }}
+          onChange={handleChange}
         />
       </ListItem>
     </>
