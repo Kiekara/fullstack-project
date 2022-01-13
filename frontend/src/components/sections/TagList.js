7aa38567bd918d26a87a03789900930a8fb644a1
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 02:38:56 +0200

    Add tag mapping on return.

--- frontend/src/components/sections/TagList.js
+++ frontend/src/components/sections/TagList.js
@@ -1,7 +1,17 @@
 import React from "react";
+import TagButton from "../items/TagButton";
+import { Stack } from "@mui/material";
 
-function TagList() {
-  return <div></div>;
+function TagList({ tags }) {
+  return (
+    <>
+      <Stack spacing={2}>
+        {tags.map((tag) => (
+          <TagButton tag={tag} />
+        ))}
+      </Stack>
+    </>
+  );
 }
 
 export default TagList;
