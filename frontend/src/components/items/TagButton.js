93c13c6c592ad3ad34ce065a647de12003f78ce5
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 03:49:20 +0200

    Add function to change sort value on click.

--- frontend/src/components/items/TagButton.js
+++ frontend/src/components/items/TagButton.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Button } from "@mui/material";
 
-function TagButton({ tag }) {
+function TagButton({ tag, setSort }) {
   const { id, name } = tag;
+
+  const handleClick = () => {
+    setSort(id);
+  };
 
   return (
     <>
-      <Button variant="contained" size="medium">
+      <Button variant="contained" size="medium" onClick={handleClick}>
         Learn {name}
       </Button>
     </>
