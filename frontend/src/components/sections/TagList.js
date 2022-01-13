976aee6451f98e1ac0cd11e122a34d7b41911db4
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 04:54:17 +0200

    Call LearnButton() component on return.

--- frontend/src/components/sections/TagList.js
+++ frontend/src/components/sections/TagList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import TagButton from "../items/TagButton";
 import BackButton from "../items/BackButton";
+import LearnButton from "../items/LearnButton";
 import { Stack } from "@mui/material";
 
 function TagList({ tags, learn, setSort, setLearn }) {
@@ -9,6 +10,7 @@ function TagList({ tags, learn, setSort, setLearn }) {
       <Stack spacing={2}>
         <h3>Choose a category</h3>
         <BackButton learn={learn} setSort={setSort} setLearn={setLearn} />
+        <LearnButton />
         {tags.map((tag) => (
           <TagButton
             tag={tag}
