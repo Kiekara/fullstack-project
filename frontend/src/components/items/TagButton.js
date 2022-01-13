cca75dfd76dd05ac2500add6b25d7dbc3a933abe
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 02:29:39 +0200

    Return Button component with tag name.

--- frontend/src/components/items/TagButton.js
+++ frontend/src/components/items/TagButton.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button } from "@mui/material";
 
-function TagButton() {
-  return <div></div>;
+function TagButton({ tag }) {
+  const { id, name } = tag;
+
+  return (
+    <>
+      <Button variant="contained" size="medium">
+        Learn {name}
+      </Button>
+    </>
+  );
 }
 
 export default TagButton;
