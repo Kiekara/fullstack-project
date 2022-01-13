a2d635fecbc61112f76ada8df5c4ee14b109c7d7
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 00:54:03 +0200

    Remove conditional button disabling.

--- frontend/src/components/items/BackButton.js
+++ frontend/src/components/items/BackButton.js
@@ -3,26 +3,14 @@ import { Button } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
-function BackButton({ learn, setSort, setLearn, setSubmit }) {
+function BackButton({ setSort, setLearn, setSubmit }) {
   const handleClick = () => {
     setSort(-1);
     setLearn(false);
     setSubmit(false);
   };
 
-  return !learn ? (
-    <>
-      <Button
-        disabled
-        variant="contained"
-        size="medium"
-        startIcon={<FontAwesomeIcon icon={faChevronLeft} />}
-        onClick={handleClick}
-      >
-        Go back
-      </Button>
-    </>
-  ) : (
+  return (
     <>
       <Button
         variant="contained"
