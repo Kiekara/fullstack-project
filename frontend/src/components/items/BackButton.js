14545ee8a9073634603f500b708f1fcb58881f86
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-13 04:33:32 +0200

    Add function to reset sort value on click.

--- frontend/src/components/items/BackButton.js
+++ frontend/src/components/items/BackButton.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Button } from "@mui/material";
 
-function BackButton() {
+function BackButton({ setSort }) {
+  const handleClick = () => {
+    setSort(-1);
+  };
+
   return (
     <>
-      <Button disabled variant="contained" size="medium">
+      <Button disabled variant="contained" size="medium" onClick={handleClick}>
         Go back
       </Button>
     </>
