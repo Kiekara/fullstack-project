22759bbe995406764b899457ceb7deb1b5bf0217
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 06:01:50 +0200

    Add TextFields with IconButtons in return.

--- frontend/src/components/items/EditWordRow.js
+++ frontend/src/components/items/EditWordRow.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { IconButton, ListItem, TextField } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-function EditWordRow() {
-  return <div></div>;
+function EditWordRow({ row, swap, setRowEdit, getData, api }) {
+  return (
+    <ListItem>
+      <TextField
+        hiddenLabel
+        variant="filled"
+        size="small"
+        margin="dense"
+        sx={{ width: "41%", mr: "8px" }}
+      />
+      <TextField
+        hiddenLabel
+        variant="filled"
+        size="small"
+        margin="dense"
+        sx={{ width: "41%", mr: "8px" }}
+      />
+      <span style={{ paddingLeft: "8px" }}>
+        <IconButton size="medium" color="error">
+          <FontAwesomeIcon icon={faTimes} />
+        </IconButton>
+        <IconButton size="medium" color="success">
+          <FontAwesomeIcon icon={faCheck} />
+        </IconButton>
+      </span>
+    </ListItem>
+  );
 }
 
 export default EditWordRow;
