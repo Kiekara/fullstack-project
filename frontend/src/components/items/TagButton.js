92f85b9bed829a8d33ea1ce00900c92e460cd6df
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 23:21:11 +0200

    Add rights to render only relevant buttons.

--- frontend/src/components/items/TagButton.js
+++ frontend/src/components/items/TagButton.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
 
-function TagButton({ tag, learn, setSort, setLearn }) {
+function TagButton({ rights, tag, words, learn, setSort, setLearn }) {
   const { id, name } = tag;
+  const [numOfProps, setNumOfProps] = useState(
+    words.filter((row) => row.tagID === id).length
+  );
 
   const handleClick = () => {
+    console.log(numOfProps);
     setSort(id);
     setLearn(true);
   };
 
-  return (
+  return (numOfProps >= 5 && rights === "user") || rights === "admin" ? (
     <>
       {!learn ? (
-        <Button variant="contained" size="medium" onClick={handleClick}>
+        <Button
+          key={id}
+          variant="contained"
+          size="medium"
+          onClick={handleClick}
+        >
           Learn {name}
         </Button>
       ) : (
-        <Button disabled variant="contained" size="medium">
+        <Button key={id} disabled variant="contained" size="medium">
           Learn {name}
         </Button>
       )}
     </>
-  );
+  ) : null;
 }
 
 export default TagButton;
