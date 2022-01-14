f56ef589ca0daef31eb04f25981a4ea4f6ff20c9
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 09:42:41 +0200

    Add function for handling input change.

--- frontend/src/components/items/EditTagForm.js
+++ frontend/src/components/items/EditTagForm.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, ListItem, TextField } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 function EditTagForm({ tag, getData, api }) {
+  const [newTag, setNewTag] = useState("");
   const { id, name } = tag;
+
+  const handleChange = (event) => {
+    let inputValue = event.target.value;
+    setNewTag(inputValue);
+  };
 
   return (
     <ListItem>
       <TextField
         hiddenLabel
         placeholder={name}
+        value={name}
         size="small"
         sx={{ width: "62%", height: "36.5px", mr: "auto" }}
+        onChange={handleChange}
       />
       <Button
         variant="contained"
