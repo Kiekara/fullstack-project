ef77cccc90bbe3e7cc6b04188b53faae85f570c6
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 05:16:48 +0200

    Add function for handling input change.

--- frontend/src/components/items/AddTagForm.js
+++ frontend/src/components/items/AddTagForm.js
@@ -6,6 +6,11 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 function AddTagForm({ getData, api }) {
   const [tag, setTag] = useState("");
 
+  const handleChange = (event) => {
+    let inputValue = event.target.value;
+    setTag(inputValue);
+  };
+
   return (
     <>
       <span
@@ -17,10 +22,11 @@ function AddTagForm({ getData, api }) {
       >
         <TextField
           label="Tag"
-          value={""}
+          value={tag}
           size="small"
           margin="dense"
           sx={{ width: "85%", mr: "8px" }}
+          onChange={handleChange}
         />
         <IconButton size="medium" color="success">
           <FontAwesomeIcon icon={faPlus} />
