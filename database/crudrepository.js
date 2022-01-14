57b2fe373ae17b833a8868c2616c649f78a64808
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 23:46:46 +0200

    Change environment variable names.

--- database/crudrepository.js
+++ database/crudrepository.js
@@ -3,10 +3,10 @@ const mysql = require("mysql");
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DB,
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
 });
 
 let connectionFunctions = {
