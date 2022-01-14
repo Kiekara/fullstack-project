49ccc46868b7b215100e4062e42a5eac4d097837
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 06:20:54 +0200

    Add functions for handling input changes.

--- frontend/src/components/items/EditWordRow.js
+++ frontend/src/components/items/EditWordRow.js
@@ -3,10 +3,28 @@ import { IconButton, ListItem, TextField } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-function EditWordRow({ row, primary, secondary, setRowEdit, getData, api }) {
-  const { id, wordEng, wordFin } = row;
+function EditWordRow({
+  row,
+  swap,
+  primary,
+  secondary,
+  setRowEdit,
+  getData,
+  api,
+}) {
+  const { tagID } = row;
   const [newPri, setNewPri] = useState("");
   const [newSec, setNewSec] = useState("");
+
+  const handlePrimary = (event) => {
+    let inputValue = event.target.value;
+    setNewPri(inputValue);
+  };
+
+  const handleSecondary = (event) => {
+    let inputValue = event.target.value;
+    setNewSec(inputValue);
+  };
 
   return (
     <ListItem>
@@ -14,17 +32,21 @@ function EditWordRow({ row, primary, secondary, setRowEdit, getData, api }) {
         hiddenLabel
         placeholder={primary}
         variant="filled"
+        value={newPri}
         size="small"
         margin="dense"
         sx={{ width: "41%", mr: "8px" }}
+        onChange={handlePrimary}
       />
       <TextField
         hiddenLabel
         placeholder={secondary}
         variant="filled"
+        value={newSec}
         size="small"
         margin="dense"
         sx={{ width: "41%", mr: "8px" }}
+        onChange={handleSecondary}
       />
       <span style={{ paddingLeft: "8px" }}>
         <IconButton size="medium" color="error">
