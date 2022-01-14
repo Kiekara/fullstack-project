dc357f74dfd8c44cd0d398c4fa85cec4ef7fc895
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 07:46:37 +0200

    Add Button with text in return.

--- frontend/src/components/items/QuitButton.js
+++ frontend/src/components/items/QuitButton.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Button } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-function QuitButton() {
-  return <div></div>;
+function QuitButton({ setEdit }) {
+  return (
+    <span style={{ marginRight: "auto", marginLeft: "auto" }}>
+      <Button
+        variant="contained"
+        size="medium"
+        startIcon={<FontAwesomeIcon icon={faTimes} />}
+      >
+        Quit editing
+      </Button>
+    </span>
+  );
 }
 
 export default QuitButton;
