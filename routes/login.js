bb403ae717ccb3113e2ef557c0a86be198b41db7
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 20:45:46 +0200

    Add dummy users with admin and normal user rights.

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const Validator = require("jsonschema").Validator;
 
+const users = [
+  {
+    username: "teacher",
+    password: "1234",
+    rights: "admin",
+  },
+  {
+    username: "student",
+    password: "0000",
+    rights: "user",
+  },
+];
+
 const login = express.Router();
 const validator = new Validator();
 
