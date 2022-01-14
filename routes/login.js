3481512b04b74bd049b299047a7252f48aa30a08
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 21:58:10 +0200

    Add function for authorization status.

--- routes/login.js
+++ routes/login.js
@@ -30,15 +30,42 @@ const credentialsSchema = {
   required: ["username", "password"],
 };
 
+const authorizeLogin = (credentials) => {
+  let user = users.find((user) => user.username === credentials.username);
+
+  if (user) {
+    if (user.password === credentials.password) {
+      return { status: "OK", code: 200, response: { rights: user.rights } };
+    } else {
+      return {
+        status: "unauthorized",
+        code: 401,
+        response: { msg: "Incorrect password." },
+      };
+    }
+  } else {
+    return {
+      status: "unauthorized",
+      code: 401,
+      response: { msg: "Incorrect username or password." },
+    };
+  }
+};
+
 login.post("/", (req, res) => {
   let credentials = req.body;
   let credentialsValidation = validator.validate(
     credentials,
     credentialsSchema
   );
+  const { status, code, response } = authorizeLogin(credentials);
 
   if (credentialsValidation.errors.length > 0) {
     res.status(401).send(credentialsValidation.errors);
+  } else if (status === "unauthorized") {
+    res.status(code).send(response);
+  } else {
+    res.status(code).send(response);
   }
 });
 
