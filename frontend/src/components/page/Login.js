e48de739d709bbb9767cf8c3a9efb187dcb75b2f
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 22:21:56 +0200

    Add stylished password field.

--- frontend/src/components/page/Login.js
+++ frontend/src/components/page/Login.js
@@ -1,11 +1,39 @@
 import React, { useState } from "react";
-import { TextField } from "@mui/material";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 function Login() {
+  const [passValues, setPassValues] = useState({
+    password: "",
+    showPassword: false,
+  });
   const [username, setUsername] = useState("");
+
+  const handlePasswordChange = (prop) => (event) => {
+    setPassValues({ ...passValues, [prop]: event.target.value });
+  };
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
+  };
+
+  const handleClickShowPassword = () => {
+    setPassValues({
+      ...passValues,
+      showPassword: !passValues.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
   };
 
   return (
@@ -16,6 +44,33 @@ function Login() {
         sx={{ m: 1, mt: 4, width: "25ch", justifySelf: "center" }}
         onChange={handleUsernameChange}
       />
+      <FormControl
+        sx={{ m: 1, width: "25ch", justifySelf: "center" }}
+        variant="outlined"
+      >
+        <InputLabel>Password</InputLabel>
+        <OutlinedInput
+          type={passValues.showPassword ? "text" : "password"}
+          value={passValues.password}
+          onChange={handlePasswordChange("password")}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {passValues.showPassword ? (
+                  <FontAwesomeIcon icon={faEyeSlash} />
+                ) : (
+                  <FontAwesomeIcon icon={faEye} />
+                )}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="password"
+        />
+      </FormControl>
     </>
   );
 }
