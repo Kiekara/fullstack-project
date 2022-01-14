e22696a7693c9e427694ab91dbe665dea2f29342
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 02:18:29 +0200

    Add TextFields with IconButton on return.

--- frontend/src/components/items/WordRowForm.js
+++ frontend/src/components/items/WordRowForm.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { IconButton, ListItem, TextField } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function WordRowForm() {
-  return <div></div>;
+  return (
+    <ListItem>
+      <TextField
+        hiddenLabel
+        variant="filled"
+        size="small"
+        margin="dense"
+        sx={{ width: "45%", mr: "8px" }}
+      />
+      <TextField
+        hiddenLabel
+        variant="filled"
+        size="small"
+        margin="dense"
+        sx={{ width: "45%", mr: "8px" }}
+      />
+      <IconButton size="medium" color="success">
+        <FontAwesomeIcon icon={faPlus} />
+      </IconButton>
+    </ListItem>
+  );
 }
 
 export default WordRowForm;
