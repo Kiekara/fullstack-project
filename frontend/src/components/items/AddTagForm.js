64fe433e395301b185e801ac2ef4abca355ce340
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 05:09:28 +0200

    Add TextField with IconButton on return.

--- frontend/src/components/items/AddTagForm.js
+++ frontend/src/components/items/AddTagForm.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { IconButton, TextField } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function AddTagForm() {
-  return <div></div>;
+  return (
+    <>
+      <span
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <TextField
+          label="Tag"
+          value={""}
+          size="small"
+          margin="dense"
+          sx={{ width: "85%", mr: "8px" }}
+        />
+        <IconButton size="medium" color="success">
+          <FontAwesomeIcon icon={faPlus} />
+        </IconButton>
+      </span>
+    </>
+  );
 }
 
 export default AddTagForm;
