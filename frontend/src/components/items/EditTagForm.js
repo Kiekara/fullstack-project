3f94289b55848775a05f35e75df4f23b46313369
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-01-14 09:40:08 +0200

    Add TextField with Button on return.

--- frontend/src/components/items/EditTagForm.js
+++ frontend/src/components/items/EditTagForm.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Button, ListItem, TextField } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
-function EditTagForm() {
-  return <div></div>;
+function EditTagForm({ tag, getData, api }) {
+  const { id, name } = tag;
+
+  return (
+    <ListItem>
+      <TextField
+        hiddenLabel
+        placeholder={name}
+        size="small"
+        sx={{ width: "62%", height: "36.5px", mr: "auto" }}
+      />
+      <Button
+        variant="contained"
+        size="medium"
+        color="success"
+        startIcon={<FontAwesomeIcon icon={faCheck} />}
+      >
+        Save changes
+      </Button>
+    </ListItem>
+  );
 }
 
 export default EditTagForm;
